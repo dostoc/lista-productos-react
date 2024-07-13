e4a4c588c55b150dd117e2b5324261d3fb1d54fb
Jonatan Postemsky <jona.ky@gmail.com>
2024-07-12 23:40:13 -0300

    pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://dostoc.github.io/lista-productos-react',
+  base:"https://dostoc.github.io/lista-productos-react/",
 
 })
