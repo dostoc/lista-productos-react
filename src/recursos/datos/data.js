c52d0501fe5018d19a2e26c602dc753990baab89
Jonatan Postemsky <jona.ky@gmail.com>
2024-07-15 00:30:36 -0300

    ajuste de imagenes

--- src/recursos/datos/data.js
+++ src/recursos/datos/data.js
@@ -7,7 +7,7 @@ export const data = [
         nombre: 'CS-600B',
         marca: 'DIRUI',
         detalle: 'El CS-600B es un autoanalizador de alto rendimiento. Procesa 600 tests fotométricos por hora con la posibilidad opcional de sumar un módulo ISE (1000 test por hora). Utiliza tres brazos para la dispersión de reactivos y muestras.',
-        imageUrl: './src/recursos/img/01.png',
+        imageUrl: 'https://raw.githubusercontent.com/dostoc/lista-productos-react/main/src/recursos/img/01.png',
         ImageAlt: 'Analizador de química clínica CS-T240',
     },
     {
@@ -16,7 +16,7 @@ export const data = [
         nombre: 'CS-400',
         marca: 'DIRUI',
         detalle: 'Este es un autoanalizador de alto rendimiento ideal para Laboratorios medianos. Tiene un rendimiento constante de 400 test fotométricos por hora con la posibilidad de un rendimiento máximo de 800 test por hora con una unidad ISE (K, Na, Cl). ',
-        imageUrl: './src/recursos/img/02.png',
+        imageUrl: 'https://raw.githubusercontent.com/dostoc/lista-productos-react/main/src/recursos/img/02.png',
         imageAlt: 'CS-400 autoanalizador',
     },
     {
@@ -25,7 +25,7 @@ export const data = [
         nombre: 'CS-300B',
         marca: 'DIRUI',
         detalle: 'El CS-300B es un autoanalizador de química clínica que procesa 300 test fotométricos por hora con la opción de incorporar ISE. Cómo toda la línea CS de Dirui tiene un sistema abierto de reactivos que permite utilizar cualquier marca de reactivos. ',
-        imageUrl: './src/recursos/img/03.png',
+        imageUrl: 'https://raw.githubusercontent.com/dostoc/lista-productos-react/main/src/recursos/img/03.png',
         imageAlt: 'CS-300B es un autoanalizador de química clínica',
     },
     {
@@ -34,7 +34,7 @@ export const data = [
         nombre: 'CS T240',
         marca: 'DIRUI',
         detalle: 'El CS-T240 es un autoanalizador de química clínica sólido y robusto que sólo necesita un mantenimiento preventivo cada 24 meses.',
-        imageUrl: './src/recursos/img/04.png',
+        imageUrl: 'https://raw.githubusercontent.com/dostoc/lista-productos-react/main/src/recursos/img/04.png',
         imageAlt: 'CS-T240 autoanalizador de química clínica',
     },
 ]
